3c949c5f10f753da05347983edf2ada408b81744
B_n <78348022+boonblablabla@users.noreply.github.com>
2023-07-03 12:22:37 +0700

    Revert to original Cat.java

--- src/myapp/Cat.java
+++ src/myapp/Cat.java
@@ -1,11 +1,4 @@
 package myapp;
 
 public class Cat {
-    private String name;
-    private int age;
-
-    public Cat(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
 }
