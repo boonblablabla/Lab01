b49cc1ae11cf976cc40ea4d294c7257168754ee7
B_n <78348022+boonblablabla@users.noreply.github.com>
2023-07-03 12:14:20 +0700

    Update Cat, add Dog

--- src/myapp/Cat.java
+++ src/myapp/Cat.java
@@ -1,5 +1,11 @@
 package myapp;
 
 public class Cat {
+    private String name;
+    private int age;
 
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
